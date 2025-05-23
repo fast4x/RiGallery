1075eef676d8b7a7ec7766c45bbbfae5d9b324c7
fast4x <rinorusso@gmail.com>
2025-05-23 23:47:26 +0200

    Update app version to 0.1.2
    - Version code changed from 2 to 3.
    - Version name changed from "0.1.1" to "0.1.2".
    - Version file updated to reflect the new version "3-v0.1.2-RiGallery".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "it.fast4x.rigallery"
         minSdk = 30
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         buildConfigField("String", "APPLICATION_NAME", "\"$applicationName\"")
 
