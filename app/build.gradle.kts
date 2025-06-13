db40fa99aa84feda446d1cc0a42061bbf2b93ce8
fast4x <rinorusso@gmail.com>
2025-06-13 22:08:19 +0200

    Update app version to 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "it.fast4x.rigallery"
         minSdk = 30
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
 
         buildConfigField("String", "APPLICATION_NAME", "\"$applicationName\"")
 
